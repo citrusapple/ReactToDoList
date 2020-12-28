4716ea5d0aed38f3d7315efaea93f23c8d475432
Wing <wng2014@gmail.com>
2020-12-27 22:43:42 -0800

    npx create-react-app . then manipulate app.js to start implementing components.  made an unordered list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>kiki</li>
+        <li>Jiji</li>
+        <li>Lala</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
