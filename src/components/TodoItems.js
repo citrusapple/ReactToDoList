6df6e7203b60675f08981583b4d8442f1c1a699e
Wing <wng2014@gmail.com>
2020-12-28 23:21:33 -0800

    learning styling quirks in jsx, added a css sheet style.css with a snippet from BBBootstrap Team, have not cut down on the style sheet quite yet

---
+++ src/components/TodoItems.js
@@ -0,0 +1,14 @@
+function TodoItems(){
+    const styles = {
+        color: "#45e69d"
+    };
+    return(
+        <div>
+            <input type="checkbox" id="todochecks1">
+            </input>
+            <p style={styles}> stuff</p>
+        </div>
+    );
+}
+
+export default TodoItems
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,12 @@
+import TodoItems from './TodoItems'
+
 function ToDoList(){
     return (
         <div>
-            <input type="checkbox" id="todochecks1">
-            </input>
-            <p>item 1</p>
-            <input type="checkbox" id="todochecks2">
-            </input>
-            <p>item 2</p>
-            <input type="checkbox" id="todochecks3">
-            </input>
-            <p>item 3</p>
+            <TodoItems />
+            <TodoItems />
+            <TodoItems />
+            <TodoItems />
         </div>
     )
 }
